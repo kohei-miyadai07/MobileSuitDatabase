4cb2eac1c698960aa28417c1848248f1274994ff
kohei-miyadai07 <mk26st57@jcom.zaq.ne.jp>
2022-02-19 11:25:05 +0900

    feat: プロジェクト追加
    refs #mqin

---
+++ MobileSuitDatabase/src/main/java/jp/co/sunarch/mobilesuitDatabase/MobileSuitDatabaseApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sunarch.mobilesuitDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileSuitDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileSuitDatabaseApplication.class, args);
+	}
+
+}
---
+++ MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/MobileSuitDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sunarch.mobilesuitDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileSuitDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
