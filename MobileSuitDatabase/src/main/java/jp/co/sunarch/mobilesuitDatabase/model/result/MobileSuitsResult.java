f9e2a195cf7b106f6ade6b5c737bf1ba40ec5a51
kohei-miyadai07 <mk26st57@jcom.zaq.ne.jp>
2022-08-11 09:32:18 +0900

    feat: modelのResult作成
    refs #002

--- MobileSuitDatabase/src/main/java/jp/co/sunarch/mobilesuitDatabase/model/result/MobileSuitsResult.java
+++ MobileSuitDatabase/src/main/java/jp/co/sunarch/mobilesuitDatabase/model/result/MobileSuitsResult.java
@@ -9,6 +9,8 @@ public class MobileSuitsResult {
 	private String msId;
 	private String modelNumber;
 	private String msName;
+	private String msUrl;
+	private String headHeight;
 	private String insertDate;
 	private String updateDate;
 
