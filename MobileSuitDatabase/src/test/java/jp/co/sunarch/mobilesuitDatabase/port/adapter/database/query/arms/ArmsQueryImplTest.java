d473d8a825b554fa1cf34a300e51ac0c1d1eca34
kohei-miyadai07 <mk26st57@jcom.zaq.ne.jp>
2023-03-29 01:01:24 +0900

    test: テストクラス初作成
    refs #004

---
+++ MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/query/arms/ArmsQueryImplTest.java
@@ -0,0 +1,16 @@
+package jp.co.sunarch.mobilesuitDatabase.port.adapter.database.query.arms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+class ArmsQueryImplTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
