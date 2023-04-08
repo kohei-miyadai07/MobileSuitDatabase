d609d1019038ab96aa6b4a2e8d24072d87326a9d
kohei-miyadai07 <mk26st57@jcom.zaq.ne.jp>
2023-04-08 22:48:22 +0900

    test: EquipmentRepositoryImplTest作成完了
    refs #004

--- MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/repository/mobilesuit/equipment/EquipmentRepositoryImplTest.java
+++ MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/repository/mobilesuit/equipment/EquipmentRepositoryImplTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -72,7 +73,7 @@ class EquipmentRepositoryImplTest {
 			from
 			Equipment 
 			where
-			ms_id = :msId
+			ms_id = ?
 			""";
 
 	private final String SELECT_EQUIPMENT_BY_ARMSID = """
@@ -84,7 +85,7 @@ class EquipmentRepositoryImplTest {
 			from
 			Equipment 
 			where
-			arms_id = :armsId;
+			arms_id = ?;
 			""";
 
 	private final String SELECT_EQUIPMENT_BY_MSID_AND_ARMSID = """
@@ -96,9 +97,9 @@ class EquipmentRepositoryImplTest {
 			from
 			Equipment 
 			where
-			ms_id = :msId
+			ms_id = ?
 			and
-			arms_id = :armsId;
+			arms_id = ?;
 			""";
 
 	@Autowired
@@ -462,7 +463,316 @@ class EquipmentRepositoryImplTest {
 
 		@Test
 		void 対象の装備ドメインモデルが未登録の場合は新規登録されること() {
-			
+			Equipment equipment = Equipment.create(
+					MobileSuitId.of("ms1"),
+					ArmsId.of("arms2"),
+					Integer.valueOf(10), "テスト装備1-2");
+			sut.save(equipment);
+
+			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
+			Equipment extend = jdbcTemplate.queryForObject(
+					SELECT_EQUIPMENT_BY_MSID_AND_ARMSID, rowMapper, "ms1", "arms2");
+
+			assertThat(equipment)
+			.isEqualTo(extend);
+		}
+
+		@Test
+		void 装備ドメインモデルが登録済みの場合は更新されること() {
+			Equipment equipment = Equipment.create(
+					MobileSuitId.of("ms1"),
+					ArmsId.of("arms1"),
+					Integer.valueOf(100), "テスト装備1_update");
+			sut.save(equipment);
+
+			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
+			Equipment extend = jdbcTemplate.queryForObject(
+					SELECT_EQUIPMENT_BY_MSID_AND_ARMSID, rowMapper, "ms1", "arms1");
+
+			assertThat(equipment)
+			.isEqualTo(extend);
+		}
+	}
+
+	@Nested
+	class DeleteEquipmentByMsIdAndArmsId {
+		@BeforeEach
+		void setUp() throws Exception {
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms1",
+					"msNum1",
+					"テストモビルスーツ1",
+					"/ms/url1",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース1",
+					"テストマテリアル1",
+					100L,
+					200L,
+					300L,
+					"テスト説明1",
+					"テスト活躍1",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms2",
+					"msNum2",
+					"テストモビルスーツ2",
+					"/ms/url2",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース2",
+					"テストマテリアル2",
+					100L,
+					200L,
+					300L,
+					"テスト説明2",
+					"テスト活躍2",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms3",
+					"msNum3",
+					"テストモビルスーツ3",
+					"/ms/url3",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース3",
+					"テストマテリアル3",
+					100L,
+					200L,
+					300L,
+					"テスト説明3",
+					"テスト活躍3",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1");
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2");
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3");
+
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3");
+		}
+
+		@Test
+		void 指定したモビルスーツIDと武器IDに紐づいた装備データが削除されていること() {
+			sut.deleteEquipmentByMsIdAndArmsId("ms3", "arms3");
+
+			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
+			Equipment extend = new Equipment();
+
+			try {
+				extend = jdbcTemplate.queryForObject(SELECT_EQUIPMENT_BY_MSID_AND_ARMSID, rowMapper, "ms3", "arms3");
+			} catch(EmptyResultDataAccessException e) {
+				extend = null;
+			}
+
+			assertThat(extend)
+			.isNull();
+		}
+	}
+
+	@Nested
+	class DeleteEquipmentByMsid {
+		@BeforeEach
+		void setUp() throws Exception {
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms1",
+					"msNum1",
+					"テストモビルスーツ1",
+					"/ms/url1",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース1",
+					"テストマテリアル1",
+					100L,
+					200L,
+					300L,
+					"テスト説明1",
+					"テスト活躍1",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms2",
+					"msNum2",
+					"テストモビルスーツ2",
+					"/ms/url2",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース2",
+					"テストマテリアル2",
+					100L,
+					200L,
+					300L,
+					"テスト説明2",
+					"テスト活躍2",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms3",
+					"msNum3",
+					"テストモビルスーツ3",
+					"/ms/url3",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース3",
+					"テストマテリアル3",
+					100L,
+					200L,
+					300L,
+					"テスト説明3",
+					"テスト活躍3",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1");
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2");
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3");
+
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3");
+		}
+
+		@Test
+		void 指定したモビルスーツIDに紐づいた装備データが削除されていること() {
+			sut.deleteEquipmentByMsid("ms3");
+
+			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
+			Equipment extend = new Equipment();
+
+			try {
+				extend = jdbcTemplate.queryForObject(SELECT_EQUIPMENT_BY_MSID, rowMapper, "ms3");
+			} catch(EmptyResultDataAccessException e) {
+				extend = null;
+			}
+
+			assertThat(extend)
+			.isNull();
+		}
+	}
+
+	@Nested
+	class DeleteEquipmentByArmsId {
+		@BeforeEach
+		void setUp() throws Exception {
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms1",
+					"msNum1",
+					"テストモビルスーツ1",
+					"/ms/url1",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース1",
+					"テストマテリアル1",
+					100L,
+					200L,
+					300L,
+					"テスト説明1",
+					"テスト活躍1",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms2",
+					"msNum2",
+					"テストモビルスーツ2",
+					"/ms/url2",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース2",
+					"テストマテリアル2",
+					100L,
+					200L,
+					300L,
+					"テスト説明2",
+					"テスト活躍2",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(
+					INSERT_MOBILESUIT,
+					"ms3",
+					"msNum3",
+					"テストモビルスーツ3",
+					"/ms/url3",
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					new BigDecimal("18.55"),
+					"テストパワーソース3",
+					"テストマテリアル3",
+					100L,
+					200L,
+					300L,
+					"テスト説明3",
+					"テスト活躍3",
+					timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"),
+					1);
+
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1");
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2");
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3");
+
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3");
+		}
+
+		@Test
+		void 指定した武器IDに紐づいた装備データが削除されていること() {
+			sut.deleteEquipmentByArmsId("arms3");
+
+			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
+			Equipment extend = new Equipment();
+
+			try {
+				extend = jdbcTemplate.queryForObject(SELECT_EQUIPMENT_BY_ARMSID, rowMapper, "arms3");
+			} catch(EmptyResultDataAccessException e) {
+				extend = null;
+			}
+
+			assertThat(extend)
+			.isNull();
 		}
 	}
 
