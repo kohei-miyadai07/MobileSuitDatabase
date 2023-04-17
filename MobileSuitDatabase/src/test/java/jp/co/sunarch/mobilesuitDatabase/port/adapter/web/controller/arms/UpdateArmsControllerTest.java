e4397ce65f4280eceeef0005e70e1cb44fa84a63
kohei-miyadai07 <mk26st57@jcom.zaq.ne.jp>
2023-04-18 00:32:02 +0900

    test: UpdateArmsControllerTest実装
    refs #004

---
+++ MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/web/controller/arms/UpdateArmsControllerTest.java
@@ -0,0 +1,92 @@
+package jp.co.sunarch.mobilesuitDatabase.port.adapter.web.controller.arms;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import jp.co.sunarch.mobilesuitDatabase.application.command.arms.UpdateArmsCommand;
+import jp.co.sunarch.mobilesuitDatabase.application.usecase.arms.UpdateArmsUseCase;
+import jp.co.sunarch.mobilesuitDatabase.domain.model.arms.ArmsId;
+import jp.co.sunarch.mobilesuitDatabase.port.adapter.web.form.arms.UpdateArmsForm;
+import jp.co.sunarch.mobilesuitDatabase.port.adapter.web.model.arms.ArmsModel;
+
+@WebMvcTest(UpdateArmsController.class)
+class UpdateArmsControllerTest {
+
+	@Autowired
+	MockMvc mvc;
+
+	@MockBean
+	ArmsQuery armsQuery;
+
+	@MockBean
+	UpdateArmsUseCase updateArmsUseCase;
+
+	@Test
+	void 武器更新画面に遷移すること() throws Exception {
+		ArmsModel armsModel = ArmsModel.builder()
+				.armsId("test_arms")
+				.armsName("test_arms_name")
+				.detail("テスト")
+				.build();
+
+		UpdateArmsForm form = UpdateArmsForm.ModelToForm(armsModel);
+
+		when(armsQuery.getArmsById(any())).thenReturn(armsModel);
+
+		mvc.perform(get("/MSDB/Arms/test_arms/edit"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("/MSDB/Arms/armsId/edit/ArmsEdit"))
+		.andExpect(model().attribute("armsEditForm", form));
+
+		verify(armsQuery).getArmsById("test_arms");
+	}
+
+	@Test
+	void 武器データを更新して一覧画面に遷移すること() throws Exception {
+		UpdateArmsCommand command = UpdateArmsCommand.builder()
+				.armsId(ArmsId.of("test_arms"))
+				.armsName("test_arms_name_update")
+				.detail("テスト更新")
+				.build();
+
+		List<ArmsModel> list = Arrays.asList(
+				ArmsModel.builder()
+				.armsId("test_arms")
+				.armsName("test_arms_name_update")
+				.detail("テスト更新")
+				.build(),
+				ArmsModel.builder()
+				.armsId("test_arms_a")
+				.armsName("test_arms_a")
+				.detail("テストA")
+				.build());
+
+		doNothing().when(updateArmsUseCase).execute(any());
+		when(armsQuery.getArmsList()).thenReturn(list);
+
+		mvc.perform(post("/MSDB/Arms/test_arms/edit-update")
+				.param("armsId", "test_arms")
+				.param("armsName", "test_arms_name_update")
+				.param("detail", "テスト更新"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("/MSDB/Arms/ArmsList"))
+		.andExpect(model().attribute("armsList", list));
+
+		verify(updateArmsUseCase).execute(command);
+		verify(armsQuery).getArmsList();
+	}
+
+}
