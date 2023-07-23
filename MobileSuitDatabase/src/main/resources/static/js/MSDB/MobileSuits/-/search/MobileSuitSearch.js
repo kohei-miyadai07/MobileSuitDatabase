d3da479bed72c05636c5553aaeebc9b0f74fcd61
kohei-miyadai07 <km57rf93@jcom.zaq.ne.jp>
2023-07-23 09:44:32 +0900

    refactor: バリデーションチェックプレ実装
    refs #499

---
+++ MobileSuitDatabase/src/main/resources/static/js/MSDB/MobileSuits/-/search/MobileSuitSearch.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // フォーム入力値が変更されたときにバリデーションを実行する
+  const form = document.getElementById("msSearchForm");
+  const modelNumberInput = document.getElementById("modelNumber");
+
+  form.addEventListener("input", function () {
+    validateForm();
+  });
+
+  function validateForm() {
+    // 初期化
+    clearErrors();
+
+    const modelNumber = modelNumberInput.value.trim();
+
+    let hasError = false;
+
+    // 型式番号のバリデーション
+    if (modelNumber === "" || modelNumber.length >= 50) {
+      showError(
+        modelNumberInput,
+        "modelNumberError",
+        "型式番号は空欄でなく、50文字以下で入力してください。"
+      );
+      hasError = true;
+    }
+
+    // 検索ボタンの活性/非活性を設定
+    const button = document.getElementById("search");
+    button.disabled = hasError;
+  }
+
+  function showError(inputElement, errorElementId, message) {
+    inputElement.classList.add("is-invalid");
+    document.getElementById(errorElementId).textContent = message;
+  }
+
+  function clearErrors() {
+    const invalidInputs = form.querySelectorAll(".is-invalid");
+    invalidInputs.forEach((input) => input.classList.remove("is-invalid"));
+    const errorElements = form.querySelectorAll(".invalid-feedback");
+    errorElements.forEach((element) => (element.textContent = ""));
+  }
+
+  // 初期表示でバリデーションチェックを実行
+  validateForm();
+});
