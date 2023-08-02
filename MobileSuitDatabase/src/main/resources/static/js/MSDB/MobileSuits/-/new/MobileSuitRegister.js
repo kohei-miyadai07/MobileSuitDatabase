3f464d05791c1f3f2f273f5111a8603a4bc5c560
kohei-miyadai07 <km57rf93@jcom.zaq.ne.jp>
2023-08-02 22:04:42 +0900

    refactor: モビルスーツ登録画面バリエーションチェック実装
    refs #006

--- MobileSuitDatabase/src/main/resources/static/js/MSDB/MobileSuits/-/new/MobileSuitRegister.js
+++ MobileSuitDatabase/src/main/resources/static/js/MSDB/MobileSuits/-/new/MobileSuitRegister.js
@@ -4,6 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const msNameInput = document.getElementById("msName");
   const modelNumberInput = document.getElementById("modelNumber");
+  const headHeightInput = document.getElementById("headHeight");
+  const overallHeightInput = document.getElementById("overallHeight");
+  const weightInput = document.getElementById("weight");
+  const totalWeightInput = document.getElementById("totalWeight");
+  const powerSourceInput = document.getElementById("powerSource");
+  const materialInput = document.getElementById("material");
+  const effectiveSensorRadiusInput = document.getElementById(
+    "effectiveSensorRadius"
+  );
+  const generatorOutputInput = document.getElementById("generatorOutput");
+  const totalThrustersOutputInput = document.getElementById(
+    "totalThrustersOutput"
+  );
 
   form.addEventListener("input", function () {
     validateForm();
@@ -15,6 +28,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const msName = msNameInput.value.trim();
     const modelNumber = modelNumberInput.value.trim();
+    const headHeight = headHeightInput.value.trim();
+    const overallHeight = overallHeightInput.value.trim();
+    const weight = weightInput.value.trim();
+    const totalWeight = totalWeightInput.value.trim();
+    const powerSource = powerSourceInput.value.trim();
+    const material = materialInput.value.trim();
+    const effectiveSensorRadius = effectiveSensorRadiusInput.value.trim();
+    const generatorOutput = generatorOutputInput.value.trim();
+    const totalThrustersOutput = totalThrustersOutputInput.value.trim();
 
     let hasError = false;
 
@@ -35,13 +57,111 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 型式番号のバリデーション
     if (modelNumber === "") {
-        showError(modelNumberInput, "modelNumberError", "型式番号は必須入力です。");
-        hasError = true;
+      showError(
+        modelNumberInput,
+        "modelNumberError",
+        "型式番号は必須入力です。"
+      );
+      hasError = true;
     }
 
     if (modelNumber.length > 300) {
-        showError(modelNumberInput, "modelNumberError", "型式番号は300文字以下で入力して下さい。");
-        hasError = true;
+      showError(
+        modelNumberInput,
+        "modelNumberError",
+        "型式番号は300文字以下で入力して下さい。"
+      );
+      hasError = true;
+    }
+
+    // 頭高頂のバリデーション
+    if (!/^(\d{1,3}(\.\d{1,2})?)?$/.test(headHeight)) {
+      showError(
+        headHeightInput,
+        "headHeightError",
+        "頭高頂は整数3桁以下、小数点以下2桁までの数値を入力してください。"
+      );
+      hasError = true;
+    }
+
+    // 全高のバリデーション
+    if (!/^(\d{1,3}(\.\d{1,2})?)?$/.test(overallHeight)) {
+      showError(
+        overallHeightInput,
+        "overallHeightError",
+        "全高は整数3桁以下、小数点以下2桁までの数値を入力してください。"
+      );
+      hasError = true;
+    }
+
+    // 本体重量のバリデーション
+    if (!/^(\d{1,3}(\.\d{1,2})?)?$/.test(weight)) {
+      showError(
+        weightInput,
+        "weightError",
+        "本体重量は整数3桁以下、小数点以下2桁までの数値を入力してください。"
+      );
+      hasError = true;
+    }
+
+    // 全備重量のバリデーション
+    if (!/^(\d{1,3}(\.\d{1,2})?)?$/.test(totalWeight)) {
+      showError(
+        totalWeightInput,
+        "totalWeightError",
+        "全備重量は整数3桁以下、小数点以下2桁までの数値を入力してください。"
+      );
+      hasError = true;
+    }
+
+    // 動力源のバリデーション
+    if (powerSource.length > 300) {
+      showError(
+        powerSourceInput,
+        "powerSourceError",
+        "動力源は300文字以下で入力して下さい。"
+      );
+      hasError = true;
+    }
+
+    // 装甲材質のバリデーション
+    if (material.length > 300) {
+      showError(
+        materialInput,
+        "materialError",
+        "装甲材質は300文字以下で入力して下さい。"
+      );
+      hasError = true;
+    }
+
+    // センサー有効半径のバリデーション
+    if (isNaN(effectiveSensorRadius)) {
+      showError(
+        effectiveSensorRadiusInput,
+        "effectiveSensorRadiusError",
+        "センサー有効半径は数値を入力してください。"
+      );
+      hasError = true;
+    }
+
+    // ジェネレーター出力のバリデーション
+    if (isNaN(generatorOutput)) {
+      showError(
+        generatorOutputInput,
+        "generatorOutputError",
+        "ジェネレーター出力は数値を入力してください。"
+      );
+      hasError = true;
+    }
+
+    // スラスター総推力のバリデーション
+    if (isNaN(totalThrustersOutput)) {
+      showError(
+        totalThrustersOutputInput,
+        "totalThrustersOutputError",
+        "スラスター総推力は数値を入力してください。"
+      );
+      hasError = true;
     }
 
     // 検索ボタンの活性/非活性を設定
