4ec0ac835a0a2ee3c7d4d5972d6eb282e90bc52c
kohei-miyadai07 <wm25gb87@gmail.com>
2024-02-28 23:56:48 +0900

    refactor: Doma2の自動テスト対応（失敗）
    refs #011

--- MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/query/arms/ArmsQueryImplTest.java
+++ MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/query/arms/ArmsQueryImplTest.java
@@ -2,33 +2,31 @@ package jp.co.sunarch.mobilesuitDatabase.port.adapter.database.query.arms;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.seasar.doma.boot.autoconfigure.DomaAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
-import jp.co.sunarch.mobilesuitDatabase.port.adapter.query.arms.ArmsQuery;
-import jp.co.sunarch.mobilesuitDatabase.port.adapter.query.arms.ArmsQuery.Criteria;
 import jp.co.sunarch.mobilesuitDatabase.port.adapter.web.model.arms.ArmsModel;
 
 @JdbcTest
 @ActiveProfiles("test")
-@Import({ArmsQueryImpl.class, JdbcArmsDao.class})
+@Import(DomaAutoConfiguration.class)
 class ArmsQueryImplTest {
 
 	private final String INSERT_ARMS = """
 			insert 
 			into Arms 
-			values (?, ?, ?);
+			values (?, ?, ?, ?, ?, ?);
 			""";
 
 	@Autowired
@@ -39,10 +37,9 @@ class ArmsQueryImplTest {
 
 	@BeforeEach
 	void seUp() {
-		jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1");
-		jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2");
-		jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3");
-		
+		jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", Timestamp.valueOf("2019-05-01 01:02:03"), Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+		jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", Timestamp.valueOf("2019-05-01 01:02:03"), Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+		jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", Timestamp.valueOf("2019-05-01 01:02:03"), Timestamp.valueOf("2019-05-01 01:02:03"), 1);
 	}
 
 	@Nested
@@ -57,37 +54,8 @@ class ArmsQueryImplTest {
 		}
 	}
 
-	@Nested
-	class GetArmsById {
-		@Test
-		void 武器IDを指定すると紐づいた武器データが取得できること() throws Exception {
-			Optional<ArmsModel> armsOpt = sut.getArmsById("arms1");
-
-			Optional<ArmsModel> extend = Optional.ofNullable(createArmsModel(1));
-			assertThat(armsOpt)
-			.isEqualTo(extend);
-		}
-	}
-
-	@Nested
-	class SearchArms {
-		@Test
-		void 条件を指定すると紐づいた武器データが取得できること() throws Exception {
-			Criteria criteria = ArmsQuery.Criteria
-					.builder()
-					.armsName("テストライフル1")
-					.build();
-			List<ArmsModel> armsList = sut.searchArms(criteria);
-
-			List<ArmsModel> extendList = Collections.singletonList(createArmsModel(1));
-			assertThat(armsList)
-			.isEqualTo(extendList);
-		}
-	}
-
 	private List<ArmsModel> createArmsModelList() {
 		List<ArmsModel> list = new ArrayList<>();
-
 		list.add(createArmsModel(1));
 		list.add(createArmsModel(2));
 		list.add(createArmsModel(3));
@@ -100,6 +68,9 @@ class ArmsQueryImplTest {
 				.armsId("arms" + seq)
 				.armsName("テストライフル" + seq)
 				.detail("テスト" + seq)
+				.insertDate(Timestamp.valueOf("2019-05-01 01:02:03").toInstant())
+				.updateDate(Timestamp.valueOf("2019-05-01 01:02:03").toInstant())
+				.version(1)
 				.build();
 	}
 
