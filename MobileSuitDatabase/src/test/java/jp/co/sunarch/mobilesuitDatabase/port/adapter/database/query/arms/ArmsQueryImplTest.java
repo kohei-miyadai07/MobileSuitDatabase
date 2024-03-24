f287d29b9dffb36099147ee1dc1020dcf0209482
kohei-miyadai07 <wm25gb87@gmail.com>
2024-03-24 09:25:45 +0900

    test: ArmsQueryImplTest修正
    refs #011

--- MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/query/arms/ArmsQueryImplTest.java
+++ MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/query/arms/ArmsQueryImplTest.java
@@ -1,12 +1,14 @@
 package jp.co.sunarch.mobilesuitDatabase.port.adapter.database.query.arms;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sunarch.mobilesuitDatabase.port.adapter.query.arms.ArmsQuery;
+import jp.co.sunarch.mobilesuitDatabase.port.adapter.query.arms.ArmsQuery.Criteria;
 import jp.co.sunarch.mobilesuitDatabase.port.adapter.web.model.arms.ArmsModel;
 
 @SpringBootTest
@@ -23,8 +27,8 @@ import jp.co.sunarch.mobilesuitDatabase.port.adapter.web.model.arms.ArmsModel;
 class ArmsQueryImplTest {
 
 	private final String INSERT_ARMS = """
-			insert 
-			into Arms 
+			insert
+			into Arms
 			values (?, ?, ?, ?, ?, ?);
 			""";
 
@@ -34,22 +38,104 @@ class ArmsQueryImplTest {
 	@Autowired
 	private ArmsQueryImpl sut;
 
-	@BeforeEach
-	void setUp() {
-		jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", Timestamp.valueOf("2019-05-01 01:02:03"), Timestamp.valueOf("2019-05-01 01:02:03"), 1);
-		jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", Timestamp.valueOf("2019-05-01 01:02:03"), Timestamp.valueOf("2019-05-01 01:02:03"), 1);
-		jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", Timestamp.valueOf("2019-05-01 01:02:03"), Timestamp.valueOf("2019-05-01 01:02:03"), 1);
-	}
-
 	@Nested
+	@DisplayName("武器一覧取得")
 	class GetArmsList {
 		@Test
 		void テーブル全件のデータが取得されること() throws Exception {
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+
 			List<ArmsModel> armsModelList = sut.getArmsList();
 
 			List<ArmsModel> extendList = createArmsModelList();
 			assertThat(armsModelList)
-			.isEqualTo(extendList);
+					.containsExactlyElementsOf(extendList);
+		}
+
+		@Test
+		void テーブルにデータがない場合は0件であること() {
+			List<ArmsModel> armsModelList = sut.getArmsList();
+
+			assertThat(armsModelList.size()).isEqualTo(0);
+		}
+	}
+
+	@Nested
+	@DisplayName("武器取得ID指定")
+	class GetArmsById {
+		@Test
+		void 指定したIDの武器が取得できること() {
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+
+			Optional<ArmsModel> armsModelOpt = sut.getArmsById("arms1");
+
+			assertTrue(armsModelOpt.isPresent());
+
+			ArmsModel armsModel = armsModelOpt.get();
+			assertThat(armsModel.getArmsId()).isEqualTo("arms1");
+			assertThat(armsModel.getArmsName()).isEqualTo("テストライフル1");
+			assertThat(armsModel.getDetail()).isEqualTo("テスト1");
+		}
+
+		@Test
+		void 存在しないIDを指定した時はデータが存在しないこと() {
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+
+			Optional<ArmsModel> armsModelOpt = sut.getArmsById("arms4");
+
+			assertFalse(armsModelOpt.isPresent());
+		}
+	}
+
+	@Nested
+	@DisplayName("武器検索")
+	class SearchArms {
+		@Test
+		void 武器名指定() {
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+
+			Criteria criteria = ArmsQuery.Criteria.builder().armsName("テストライフル1").build();
+			List<ArmsModel> armsModels = sut.searchArms(criteria);
+
+			List<ArmsModel> extendList = List.of(createArmsModel(1));
+			assertThat(armsModels).containsExactlyElementsOf(extendList);
+		}
+
+		@Test
+		void 武器名指定なし() {
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", Timestamp.valueOf("2019-05-01 01:02:03"),
+					Timestamp.valueOf("2019-05-01 01:02:03"), 1);
+
+			Criteria criteria = ArmsQuery.Criteria.builder().build();
+			List<ArmsModel> armsModels = sut.searchArms(criteria);
+
+			List<ArmsModel> extendList = createArmsModelList();
+			assertThat(armsModels)
+					.containsExactlyElementsOf(extendList);
 		}
 	}
 
