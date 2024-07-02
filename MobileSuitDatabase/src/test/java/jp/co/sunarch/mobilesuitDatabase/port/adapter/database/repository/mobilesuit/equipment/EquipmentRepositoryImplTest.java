17b1bdbe550b5ef13f4e8e89a336e092b578b6cb
kohei-miyadai07 <wm25gb87@gmail.com>
2024-07-02 22:55:26 +0900

    feat: 装備データDB操作テスト修正
    issue-011

--- MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/repository/mobilesuit/equipment/EquipmentRepositoryImplTest.java
+++ MobileSuitDatabase/src/test/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/repository/mobilesuit/equipment/EquipmentRepositoryImplTest.java
@@ -1,29 +1,32 @@
 package jp.co.sunarch.mobilesuitDatabase.port.adapter.database.repository.mobilesuit.equipment;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sunarch.mobilesuitDatabase.domain.model.arms.ArmsId;
 import jp.co.sunarch.mobilesuitDatabase.domain.model.mobilesuit.MobileSuitId;
 import jp.co.sunarch.mobilesuitDatabase.domain.model.mobilesuit.equipment.Equipment;
 
-@JdbcTest
+@SpringBootTest
 @ActiveProfiles("test")
-@Import({EquipmentRepositoryImpl.class, EquipmentDao.class, EquipmentConverter.class})
+@Transactional
 class EquipmentRepositoryImplTest {
 
 	private final String INSERT_MOBILESUIT = """
@@ -52,15 +55,15 @@ class EquipmentRepositoryImplTest {
 			""";
 
 	private final String INSERT_ARMS = """
-			insert 
-			into Arms 
-			values (?, ?, ?);
+			insert
+			into Arms
+			values (?, ?, ?, ?, ?, ?);
 			""";
 
 	private final String INSERT_EQUIPMENT = """
-			insert 
-			into Equipment 
-			values (?, ?, ?, ?)
+			insert
+			into Equipment
+			values (?, ?, ?, ?, ?, ?, ?)
 			""";
 
 	private final String SELECT_EQUIPMENT_BY_MSID_AND_ARMSID = """
@@ -68,7 +71,10 @@ class EquipmentRepositoryImplTest {
 			ms_id
 			, arms_id
 			, number_equipment
-			, detail 
+			, detail
+			, insert_date
+			, update_date
+			, version 
 			from
 			Equipment 
 			where
@@ -84,6 +90,7 @@ class EquipmentRepositoryImplTest {
 	private EquipmentRepositoryImpl sut;
 
 	@Nested
+	@DisplayName("モビルスーツIDと武器IDを元に装備データを取得")
 	class GetEquipmentByMsIdAndArmsId {
 		@BeforeEach
 		void setUp() throws Exception {
@@ -93,10 +100,10 @@ class EquipmentRepositoryImplTest {
 					"msNum1",
 					"テストモビルスーツ1",
 					"/ms/url1",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース1",
 					"テストマテリアル1",
 					100L,
@@ -114,10 +121,10 @@ class EquipmentRepositoryImplTest {
 					"msNum2",
 					"テストモビルスーツ2",
 					"/ms/url2",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース2",
 					"テストマテリアル2",
 					100L,
@@ -135,10 +142,10 @@ class EquipmentRepositoryImplTest {
 					"msNum3",
 					"テストモビルスーツ3",
 					"/ms/url3",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース3",
 					"テストマテリアル3",
 					100L,
@@ -150,34 +157,37 @@ class EquipmentRepositoryImplTest {
 					timestampOf("2023/04/02 10:00:00"),
 					1);
 
-			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1");
-			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2");
-			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3");
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
 
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1");
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2");
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
 		}
 
 		@Test
 		void モビルスーツIDと武器IDを指定すると紐づいた装備ドメインモデルを取得できること() {
 			Equipment equipment = sut.getEquipmentByMsIdAndArmsId("ms1", "arms1");
 
-			Equipment extend = Equipment.create(
-					MobileSuitId.of("ms1"),
-					ArmsId.of("arms1"),
-					Integer.valueOf(1),
-					"テスト装備1",
-					Instant.ofEpochSecond(0),
-					Instant.ofEpochSecond(0),
-					Integer.valueOf(1));
-
-			assertThat(equipment)
-			.isEqualTo(extend);
+			assertThat(equipment.getMsId().getValue()).isEqualTo("ms1");
+			assertThat(equipment.getArmsId().getValue()).isEqualTo("arms1");
+			assertThat(equipment.getNumberEquipment().get()).isEqualTo(1);
+			assertThat(equipment.getDetail().get()).isEqualTo("テスト装備1");
+			assertThat(equipment.getInsertDate().compareTo(Instant.parse("2023-04-02T01:00:00Z")));
+			assertThat(equipment.getUpdateDate().compareTo(Instant.parse("2023-04-02T01:00:00Z")));
+			assertThat(equipment.getVersion()).isEqualTo(1);
 		}
 	}
 
 	@Nested
+	@DisplayName("装備データ登録・更新")
 	class save {
 		@BeforeEach
 		void setUp() throws Exception {
@@ -187,10 +197,10 @@ class EquipmentRepositoryImplTest {
 					"msNum1",
 					"テストモビルスーツ1",
 					"/ms/url1",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース1",
 					"テストマテリアル1",
 					100L,
@@ -208,10 +218,10 @@ class EquipmentRepositoryImplTest {
 					"msNum2",
 					"テストモビルスーツ2",
 					"/ms/url2",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース2",
 					"テストマテリアル2",
 					100L,
@@ -229,10 +239,10 @@ class EquipmentRepositoryImplTest {
 					"msNum3",
 					"テストモビルスーツ3",
 					"/ms/url3",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース3",
 					"テストマテリアル3",
 					100L,
@@ -244,13 +254,19 @@ class EquipmentRepositoryImplTest {
 					timestampOf("2023/04/02 10:00:00"),
 					1);
 
-			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1");
-			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2");
-			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3");
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
 
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1");
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2");
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
 		}
 
 		@Test
@@ -260,17 +276,22 @@ class EquipmentRepositoryImplTest {
 					ArmsId.of("arms2"),
 					Integer.valueOf(10),
 					"テスト装備1-2",
-					Instant.ofEpochSecond(0),
-					Instant.ofEpochSecond(0),
+					Instant.parse("2023-04-02T01:00:00Z"),
+					Instant.parse("2023-04-02T01:00:00Z"),
 					Integer.valueOf(1));
 			sut.save(equipment);
 
 			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
-			Equipment extend = jdbcTemplate.queryForObject(
+			Equipment actual = jdbcTemplate.queryForObject(
 					SELECT_EQUIPMENT_BY_MSID_AND_ARMSID, rowMapper, "ms1", "arms2");
 
-			assertThat(equipment)
-			.isEqualTo(extend);
+			assertThat(actual.getMsId().getValue()).isEqualTo("ms1");
+			assertThat(actual.getArmsId().getValue()).isEqualTo("arms2");
+			assertThat(actual.getNumberEquipment().get()).isEqualTo(10);
+			assertEquals(0, actual.getInsertDate().compareTo(Instant.parse("2023-04-02T01:00:00Z")));
+			assertEquals(0, actual.getUpdateDate().compareTo(Instant.parse("2023-04-02T01:00:00Z")));
+			assertThat(actual.getVersion()).isEqualTo(1);
+			
 		}
 
 		@Test
@@ -280,21 +301,26 @@ class EquipmentRepositoryImplTest {
 					ArmsId.of("arms1"),
 					Integer.valueOf(100),
 					"テスト装備1_update",
-					Instant.ofEpochSecond(0),
-					Instant.ofEpochSecond(0),
+					Instant.parse("2023-04-02T01:00:00Z"),
+					Instant.parse("2023-04-02T01:00:00Z"),
 					Integer.valueOf(2));
 			sut.save(equipment);
 
 			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
-			Equipment extend = jdbcTemplate.queryForObject(
+			Equipment actual = jdbcTemplate.queryForObject(
 					SELECT_EQUIPMENT_BY_MSID_AND_ARMSID, rowMapper, "ms1", "arms1");
 
-			assertThat(equipment)
-			.isEqualTo(extend);
+			assertThat(actual.getMsId().getValue()).isEqualTo("ms1");
+			assertThat(actual.getArmsId().getValue()).isEqualTo("arms1");
+			assertThat(actual.getNumberEquipment().get()).isEqualTo(100);
+			assertEquals(0, actual.getInsertDate().compareTo(Instant.parse("2023-04-02T01:00:00Z")));
+			assertEquals(0, actual.getUpdateDate().compareTo(Instant.parse("2023-04-02T01:00:00Z")));
+			assertThat(actual.getVersion()).isEqualTo(2);
 		}
 	}
 
 	@Nested
+	@DisplayName("モビルスーツIDと武器IDによる装備データ削除")
 	class DeleteEquipmentByMsIdAndArmsId {
 		@BeforeEach
 		void setUp() throws Exception {
@@ -304,10 +330,10 @@ class EquipmentRepositoryImplTest {
 					"msNum1",
 					"テストモビルスーツ1",
 					"/ms/url1",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース1",
 					"テストマテリアル1",
 					100L,
@@ -325,10 +351,10 @@ class EquipmentRepositoryImplTest {
 					"msNum2",
 					"テストモビルスーツ2",
 					"/ms/url2",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース2",
 					"テストマテリアル2",
 					100L,
@@ -346,10 +372,10 @@ class EquipmentRepositoryImplTest {
 					"msNum3",
 					"テストモビルスーツ3",
 					"/ms/url3",
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
-					new BigDecimal("18.55"),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
+					new BigDecimal("18.55").setScale(2, RoundingMode.DOWN),
 					"テストパワーソース3",
 					"テストマテリアル3",
 					100L,
@@ -361,13 +387,19 @@ class EquipmentRepositoryImplTest {
 					timestampOf("2023/04/02 10:00:00"),
 					1);
 
-			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1");
-			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2");
-			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3");
+			jdbcTemplate.update(INSERT_ARMS, "arms1", "テストライフル1", "テスト1", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms2", "テストライフル2", "テスト2", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_ARMS, "arms3", "テストライフル3", "テスト3", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
 
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1");
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2");
-			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3");
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms1", "arms1", 1, "テスト装備1", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms2", "arms2", 2, "テスト装備2", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
+			jdbcTemplate.update(INSERT_EQUIPMENT, "ms3", "arms3", 3, "テスト装備3", timestampOf("2023/04/02 10:00:00"),
+					timestampOf("2023/04/02 10:00:00"), 1);
 		}
 
 		@Test
@@ -375,16 +407,15 @@ class EquipmentRepositoryImplTest {
 			sut.deleteEquipmentByMsIdAndArmsId("ms3", "arms3");
 
 			EquipmentRowMapper rowMapper = new EquipmentRowMapper();
-			Equipment extend = new Equipment();
+			Equipment actual = new Equipment();
 
 			try {
-				extend = jdbcTemplate.queryForObject(SELECT_EQUIPMENT_BY_MSID_AND_ARMSID, rowMapper, "ms3", "arms3");
+				actual = jdbcTemplate.queryForObject(SELECT_EQUIPMENT_BY_MSID_AND_ARMSID, rowMapper, "ms3", "arms3");
 			} catch(EmptyResultDataAccessException e) {
-				extend = null;
+				actual = null;
 			}
 
-			assertThat(extend)
-			.isNull();
+			assertThat(actual).isNull();
 		}
 	}
 
