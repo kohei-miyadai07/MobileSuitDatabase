fb02fa2c8724f42d1b5f98ab78e5b3829267a67b
kohei-miyadai07 <wm25gb87@gmail.com>
2024-07-09 22:24:50 +0900

    feat: domaを最新にアップデート
    refs #issue-012

--- MobileSuitDatabase/src/main/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/query/arms/entity/ArmsEntity.java
+++ MobileSuitDatabase/src/main/java/jp/co/sunarch/mobilesuitDatabase/port/adapter/database/query/arms/entity/ArmsEntity.java
@@ -6,11 +6,10 @@ import org.seasar.doma.Entity;
 import org.seasar.doma.Id;
 import org.seasar.doma.Table;
 import org.seasar.doma.Version;
-import org.seasar.doma.jdbc.entity.NamingType;
 
 import lombok.Data;
 
-@Entity(naming = NamingType.LENIENT_SNAKE_LOWER_CASE)
+@Entity
 @Table(name = "Arms")
 @Data
 public class ArmsEntity {
